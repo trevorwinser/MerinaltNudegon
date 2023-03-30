22c7b496b0a7ad69b9de2fadd851f9301c025e4d
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2023-03-29 17:41:50 -0700

    Better parser - Still don't like it
    The parser I created is not my favorite. It does not allow for sentences with extra words. It is built to understand sentences like "throw goblin wall," but not "throw goblin at the wall."
    
    My goal is to create a parser that looks for the verb at the beginning of the sentence, the subject in the middle, and the item at the end. The parser should be capable of understanding sentences that do not include items.

--- app.js
+++ app.js
@@ -1,204 +1,136 @@
 let currentRoom = null;
 let rooms = [];
 let inventory = [];
+let prevword = null;
+let dictionary = ["go","north","east","south","west"];
+
+/** 
+ * 
+*/
+class Room {
+    location;
+    description;
+    entities = [];
+    actions = [];
+    constructor(location) {
+        this.location = location;
+        this.actions.pop("go");
+    }
+}
+class Entity {
+
+}
+
 function initializeRooms() {
-    start = new Room(0,"Beginner road","There's a trail from east to west, but the trail to the west seems lead to nothing.");
+    start = new Room("Beginner road");
+    start.description = "There's a trail from east to west, but the trail to the west seems lead to nothing.";
     rooms.push(start);
     currentRoom = start;
-    nothing = new Room();
-    nothing.description = "There's nothing past this point."
+    // nothing = new Room("Nothing","There's nothing past this point. Must be a bug.");
+    // beginnerfork = new Room("A fork in the road connects to a road that leads north, and ");
+    // rooms.push(beginnerfork);
+    // connectRooms(start, nothing, "W", "E");
 }
-function actionHandler(sentence) {
+/** 
+ * A sentence is structured as follows:
+ * verb subject item.
+ * A subject can be an item and vice versa. You can throw brick at goblin and throw goblin at brick.
+ * Some combinations may not be possible though. You cannot throw wall at goblin (or maybe you can)
+ * If the first word is not recognized as a verb, it checks the previous word.
+ * If the previous word is not a verb, then the program then checks if the first word is the subject
+ * If there is nothing else after the subject, it prompts the user for an action on the subject
+ */
+function parse(sentence) {
     sentence.toLowerCase();
     const words = sentence.split(" ");
-    action = words[0];
+    const word = words[0];
     words.splice(0, 1);
     subject = words.join(" ");
-    console.log(subject);
-    switch(action) {
-        case 'search':
-            search(subject);
-            break;
-        case 'attack':
-            attack(subject);
-            break;
-        case 'take':
-            grab(subject);
-            break;
-        case 'grab':
-            grab(subject);
-            break;
-        case 'pick up':
-            grab(subject);
-            break;
-        case 'enter':
-            enter(subject);
-            break;
-        case 'look':
-            look(subject);
-            break;
+
+    //Might come back to.
+    // if (words.length == 0) {                                            //There is only one word
+    //     console.log("1 word")
+    //     if (dictionary.includes(word)) {                                //The word is an action
+    //         console.log("word is action");
+    //         if (prevword != null) {
+    //             if (currentRoom.entities.includes(prevword)) {          //The previous word is an entity in the room
+    //                 parse(word.concat(" ", prevword));
+    //             } else if (currentRoom.actions.includes(word)) {        //The previous word is not an entity, so check if it is an acceptable action
+    //                 if (!handleAction(word, null)) {                    //Checks if action needs a subject
+    //                 prevword = word;
+    //                 promptSubject(word);
+    //                 }
+    //             } else {
+    //                 outputText("You cannot do that here.")
+    //             }
+    //         } else if (!handleAction(word, null)) {                    //Checks if action needs a subject
+    //             prevword = word;
+    //             promptSubject(word);
+    //         }
+    //     } else if (currentRoom.objects.includes(word)) {                //The word is an object
+    //         if (prevword != null) {
+    //             if (currentRoom.actions.includes(prevword)) {           //The previous word is an action
+    //                 if (!handleAction(prevword, word)) {                //The action is not acceptable on the subject
+    //                     prevword = word;
+    //                     promptAction(word);
+    //                 }
+    //             }
+    //         } else {                                                    //Prompt an action
+    //             outputText(`What do you want to do with ${word}`);
+    //         }
+    //     } else if (currentRoom.objects.includes(subject) && currentRoom.actions.includes(word)) {
+    //         prevword = null;
+    //         handleAction(word, subject);
+    //     } else {
+    //         outputText("I'm sorry. I don't understand.");
+    //     }
+    // }
+}
+
+
+/**
+ * The handleAction function takes in an action and subject. 
+ * If the subject is null, the function deviates to actions that are possible without a subject. 
+ * Otherwise, the function operates assuming there is a valid subject to act on. 
+ * Current implementation does not know whether actions are possible on specific objects.
+ */
+function handleAction(action, subject) {
+    return false;
+    switch (action) {
         case 'go':
-            go(subject);
+            outputText("Where do you want to go?");
             break;
-        case 'north':
-            checkNorth();
+        case currentRoom.possileDirections.includes(action):
+
             break;
-        case 'northeast':
-            checkNorthEast();
-            break;
-        case 'east':
-            checkEast();
-            break;
-        case 'southeast':
-            checkSouthEast();
-            break;
-        case 'south':
-            checkSouth();
-            break;
-        case 'southwest':
-            checkSouthWest();
-            break;
-        case 'west':
-            checkWest();
-            break;
-        case 'northwest':
-            checkNorthWest();
-            break;
-        case 'up':
-            checkUp();
-            break;
-        case 'down':
-            checkDown();
-            break;
-        default:
-            outputText("That's not a verb I recognise.");
     }
 }
 
-function attack(subject) {
-    if (currentRoom.interactables.includes(subject)) {
-        inventory.push(subject);
-        currentRoom.interactables.pop(subject)
-    } else {
-        outputText("Swinging at the air will do you no good.");
+//IMPORTANT: To use ${variable} you must use `` not '' or ""
+function promptAction(subject) {
+    str = `What do you want to do with ${subject}?`;
+    outputText(str);
+}
+function promptSubject(action) {
+    switch (action) {
+        case 'go':
+            outputText("You need to specify a compass direction.")
     }
 }
-function grab(subject) {
-    if (currentRoom.interactables.includes(subject)) {
-        inventory.push(subject);
-        currentRoom.interactables.pop(subject)
-    } else {
-        code = Math.floor(Math.random()*3);
-        switch(code) {
-            case 0:
-                outputText("You try as hard as you can, but the object you are trying to grab does not exist.");
-                break;
-            case 1:
-                outputText("Don't be rediculous, you can't see any such thing.")
-                break;
-            case 2:
-                outputText("A valiant effort, but a fruitless one nonetheless.")
-                break;
-        }
-    }
+//Implement later
+function connectRooms(x, y, xdir, ydir) {
+    x.connectedRooms.push(y);
+    x.possibleDirections.push(xdir)
+    y.connectedRooms.push(x);
+    y.possibleDirections.push(ydir)
 }
-function enter() {}
-function go(direction) {
-    switch(direction) {
-        case 'north':
-            checkNorth();
-            break;
-        case 'northeast':
-            checkNorthEast();
-            break;
-        case 'east':
-            checkEast();
-            break;
-        case 'southeast':
-            checkSouthEast();
-            break;
-        case 'south':
-            checkSouth();
-            break;
-        case 'southwest':
-            checkSouthWest();
-            break;
-        case 'west':
-            checkWest();
-            break;
-        case 'northwest':
-            checkNorthWest();
-            break;
-        case 'up':
-            checkUp();
-            break;
-        case 'down':
-            checkDown();
-            break;
-        case '':
-            outputText("Which way do you want to go?")
-            break;
-        default:
-            outputText("I don't know the word \"" + direction + "\"");
-    }
-}
-function look(subject) {
-    const words = subject.split(" ");
-    if (words[0] === "at") words.splice(0, 1);
-    subject = words.join(" ");
-
-}
-
-class Room {
-    index;
-    interactables = [];
-    connectedRooms = [];
-    discovered = false;
-    description;
-    constructor(index) {
-        this.index = index;
-    }
-}
-
-
-
-// class Interactable {
-// }
-// class Item extends Interactable {
-//     value;
-//     constructor(value) {
-//         this.value = value;
-//     }
-// }
-// class Enemy extends Interactable {
-//     health;
-//     attack;
-//     defense;
-// }
-// class Armor extends Item {
-// }
-// class Weapon extends Item {
-// }
-
-//May potentially bring back
-// function typeWriter(txt, i, p) {
-//     if (i < txt.length) {
-//         p.innerHTML += txt.charAt(i);
-//         i++;
-//         setTimeout(this.typeWriter, 50, txt, i, p);
-//     } else {
-//         currentParagraphFinished = true;
-//         console.log(currentParagraphFinished);
-//     }
-// }
-// let currentParagraphFinished = true;
 
 function outputText(txt) {
-    if (txt.length > 215) {}
     const p = document.createElement("p");
     p.innerHTML = txt;
     terminalOutput.appendChild(p);
     window.scrollTo(0, document.body.scrollHeight);
+    terminalCommand.value = "";
 }
 
 window.onload = (event) => {
@@ -209,18 +141,19 @@ window.onload = (event) => {
 const terminalOutput = document.getElementById("terminal-output");
 const terminalCommand = document.getElementById("terminal-command");
 
-terminalCommand.addEventListener("keydown", e => checkEnter(e.keyCode));
+terminalCommand.addEventListener("keydown", e => checkEnter(e));
 
 
 
 function checkEnter(k) {
-    if (k==13) {
+    if (k.keyCode==13) {
         const command = terminalCommand.value;
-        // Add the command to the output
-        const p = document.createElement("p");
-        p.innerHTML = command;
+        outputText(command);
         terminalCommand.value = "";
-        terminalOutput.appendChild(p);
-        actionHandler(command); 
+        if (command.length > 164) {
+            outputText("I'm sorry, but that command is too long.")
+        } else {
+        parse(command); 
+        }
     }
 }
