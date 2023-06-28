000204b85bd397943b4f119a26d00662b07865d9
trevorwinser <trevoman18@gmail.com>
2023-06-28 00:58:26 -0400

    Changed Parser Again!!!!
    I love changing this parser. Nothing quite like it. Anyways, I did that, and I added new rooms. I made movement between rooms more flushed as well.

--- app.js
+++ app.js
@@ -1,69 +1,111 @@
 let currentRoom = null;
 let rooms = [];
-let inventory = ["sword"];
+let inventory = [];
 let previous_verb = null;
 let previous_component1 = null;
 let previous_component2 = null;
-let dictionary = ["climb","jump","go","walk","run","north","northeast","east","southeast","south","southwest","west","northwest","attack","look"];
+let dictionary = ["attack","look","jump","grab"];
+let movementDictionary = ["climb","go","walk","run","north","northeast","east","southeast","south","southwest","west","northwest","up","down"];
+dictionary = dictionary.concat(movementDictionary);
+let basicDictionary = dictionary;
 
+
+/**
+ * Rooms should always describe the area
+ */
 class Room {
     location;
     description = "";
     components = [];
-    actions = [];
+    dictionary = basicDictionary;
     connectedRooms = [];
     directions = [];    //corresponds to the direction of connected rooms.
+    entered = false;
     constructor(location) {
         this.location = location;
     }
 }
 
-class Component {
-    actions = [];
-    responses = [];
-    name;
-    val1;
-    val2;
-    val3;
-    constructor(name, val1, val2, val3) {
-        this.name = name;
-        if (val1 == null) val1 = 0;
-        else this.val1 = val1;
-        if (val2 == null) val2 = 0;
-        else this.val2 = val2;
-        if (val3 == null) val3 = 0;
-        else this.val3 = val3;
+class CustomRoom extends Room {
+    constructor(location) {
+        super(location);
     }
 }
 
-function equals(element, name) {
-    return element.name === name;
+class Component {
+    dictionary = [];
+    responses = [];
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+class Entity extends Component {
+    health;
+    strength;
+    defense;
+}
+
+class Item extends Component {
+    damage;
 }
 
 function initializeRooms() {
-    start = new Room("Brooke Road");
-    start.description = "The road to the east looks promising, but there's nothing to the west ";
-    sword = new Component("sword");
-    start.components.push(sword);
-    goblin = new Component("goblin");
-    start.components.push(goblin);
+    starterRoad1 = new Room("Brooke Road");
+    starterRoad1.description = "The road to the east looks promising, but there's nothing to the west.";
     nothing = new Room("Nothing");
     nothing.description = "You see nothing beyond this point. You should probably head back.";
-    connectRooms(start, nothing, "west", "east");
-    rooms.push(start);
-    currentRoom = start;
+    connectRooms(starterRoad1, nothing, "west", "east");
+    currentRoom = starterRoad1;
     beginnerFork = new Room("Fork in the Road");
-    beginnerFork.description = "A fork in the road has two trails. One heads northeast, and the other goes from east to west";
-    rooms.push(beginnerFork);
-    connectRooms(beginnerFork, start, "west", "east");
+    beginnerFork.description = "A fork in the road has two trails. One heads northeast, and the other goes from east to west.";
+    connectRooms(beginnerFork, starterRoad1, "west", "east");
     goblinDoor = new Room("Mysterious Door");
     goblinDoor.description = "A big wooden door that seems to be locked.";
     connectRooms(beginnerFork, goblinDoor, "northeast", "southwest");
-    continuedRoad = new Room("Brooke Road");
-    continuedRoad.description = "";
-    connectRooms(beginnerFork, continuedRoad, "east", "west");
+    starterRoad2 = new Room("Brooke Road");
+    starterRoad2.description = "This road leads from east to west.";
+    connectRooms(beginnerFork, starterRoad2, "east", "west");
+    wildField1 = new Room("Wild Fields");
+    wildField2 = new Room("Wild Fields");
+    wildField3 = new Room("Wild Fields");
+    wildField3.description = "A trail that leads towards the mountains is northeast from here."
+    wildField4 = new Room("Wild Fields");
+    wildField5 = new Room("Wild Fields");
+    wildField5.description = "This vast meadow goes on for a while. It may be hard to know where you're going from here, so mapping it out might help."
+    wildField6 = new Room("Wild Fields");
+    wildField7 = new Room("Wild Fields");
+    wildField7.description = "The field spreads far in every direction."
+    wildField8 = new Room("Wild Fields");
+    wildField9 = new Room("Wild Fields");
+    wildField10 = new Room("Wild Fields");
+    wildField11 = new Room("Wild Fields");
+    wildField12 = new Room("Wild Fields");
+    grandTree = new Room("Grand Tree");
+    grandTree.description = "The grand tree soars to towering heights, its branches reaching outward, while its mighty trunk radiates a mesmerizing glow."
+    largeBranch = new Room("Large Branch");
+    largeBranch.description = "This branch is plenty sturdy. From here, you can see the entire field from here. Going up might make allow you to see further."
+    appleBranch = new Room("Weak Branch");
+    appleBranch.description = "As you reach this point on the tree, you spot a shimmering apple. Will you grab it?";
+    wildField13 = new Room("Wild Fields");
+    wildField14 = new Room("Wild Fields");
+    wildField15 = new Room("Wild Fields");
+    wildField16 = new Room("Wild Fields");
+    wildField17 = new Room("Wild Fields");
+    wildField18 = new Room("Wild Fields");
+    wildField19 = new Room("Wild Fields");
+    wildField20 = new Room("Wild Fields");
+    connectRooms(starterRoad2, wildField5, "east", "west");
 }
 
+// function get(list, item) {
+//     list.some(component => {if(component.name === temp) return component});
+// }
+// function equals(element, name) {
+//     return element.name === name;
+// }
+//FUNCTIONS I MIGHT USE ^^^
 
 /** 
  * A sentence is structured as follows:
@@ -74,156 +116,187 @@ function initializeRooms() {
  * If the previous word is not a verb, then the program then checks if the first word is the subject
  * If there is nothing else after the subject, it prompts the user for an action on the subject
  */
-function parse(sentence) {
+function separateCommand(sentence) {
     sentence.toLowerCase();
     const words = sentence.split(" ");
-    let verb;
-    let component1;
-    let component2;
-    let i = 0;
-    for (; i < words.length; i++) {
-        if (dictionary.includes(words[i])) {
-            verb = words[i];
-            i++;
-            break;
-        }
-    }
-
-    for (; i < words.length; i++) {
-        let temp = words[i];
-        if (currentRoom.components.some(component => component.name === temp) || inventory.includes(temp) || dictionary.includes(temp)) {
-            component1 = temp;
-            i++;
-            break;
-        }
-    }
-  
-    for (; i < words.length; i++) {
-        let temp = words[i]
-        if (currentRoom.components.some(component => component.name === temp) || inventory.includes(temp)) {
-            component2 = temp;
-            break;
-        }
-    }
-    console.log(verb);
-    console.log(component1);
-    console.log(component2);
-    if (verb != null && component1 != null && component2 != null) {
-        handleVerbSubject1Subject2(verb, component1, component2);
-    } else if (verb != null && component1 != null) {
-        handleVerbSubject(verb, component1);
-    } else if (verb != null) {
-        handleVerb(verb);
-    } else if (previous_verb != null) {
-        if (component1 != null && component2 != null) {
-            handleAction1(previous_verb, component1, component2);
-        } else if (component1 != null) {
-            handleAction2(previous_verb, component1);
-        } else {                                            //There is never a case where handleAction3(previous_verb) should happen.
-            outputText("I'm sorry, I don't understand.")
-        }
-    } else {
-        outputText("I'm sorry, I don't understand.")
-    }
+    return words;
 }
 
-//If list comparisons get to be too annoying, I will implement this
-function getFromList(list) {
-
+function parse(words) {
+    if (previous_verb != null) {
+        words.splice(0, 0,previous_verb);
+    }
+    checkSyntax(words)
+}
+function checkSyntax(words) {
+    verb = words[0];
+    if (checkVerb(verb)) {
+        if (movementDictionary.includes(verb)) {
+            handleMovement(words);
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
 }
 
 /**
- * This variation of handleAction accepts only a verb.
- * Only actions that require no components will be handled, otherwise
- * an error is reported to the user.
+ * Checks for valid action in dictionary, and room. If either is invalid, return false. Otherwise, return true.
  */
-function handleVerb(verb) {
-    switch(verb) {
-        case "go":
-            promptDirection();
+function checkVerb(verb) { 
+    if (dictionary.includes(verb)) {
+        if (currentRoom.dictionary.includes(verb)) {
+            return true;
+        }
+        outputText("You cannot do that here.");
+        return false;
+    }
+    outputText("That's not a verb I recognize");
+    return false;
+}
+
+function handleMovement(words) {
+    switch (words[0]) {
+        case 'go':
+            if (words.length > 1) {
+                words = words.slice(1);
+                handleMovement(words);
+            } else {
+                outputText("Which direction do you want to go?");
+                previous_verb = "go";
+            }
         break;
-        case "walk":
-            promptDirection();
+        case 'walk':
+            if (words.length > 1) {
+                words = words.slice(1);
+                handleMovement(words);
+            } else {
+                outputText("Which direction do you want to go?");
+                previous_verb = "go";
+            }
         break;
-        case "run":
-            promptDirection();
+        case 'run':
+            if (words.length > 1) {
+                words = words.slice(1);
+                handleMovement(words);
+            } else {
+                outputText("Which direction do you want to go?");
+                previous_verb = "go";
+            }
         break;
-        case "north":
-            handleDirection("north");
+        case 'north':
+            if (words.length > 1) {
+                outputText("I only understod you as far as north");
+            } else {
+                handleDirection("north");
+            }
         break;
-        case "northeast":
-            handleDirection("northeast");
+        case 'northeast':
+            if (words.length > 1) {
+                outputText("I only understod you as far as northeast");
+            } else {
+                handleDirection("northeast");
+            }
         break;
-        case "east":
-            handleDirection("east");
+        case 'east':
+            if (words.length > 1) {
+                outputText("I only understod you as far as east");
+            } else {
+                handleDirection("east");
+            }
         break;
-        case "southeast":
-            handleDirection("southeast");
+        case 'southeast':
+            if (words.length > 1) {
+                outputText("I only understod you as far as southeast");
+            } else {
+                handleDirection("southeast");
+            }
         break;
-        case "south":
-            handleDirection("south");
+        case 'south':
+            if (words.length > 1) {
+                outputText("I only understod you as far as south");
+            } else {
+                handleDirection("south");
+            }
         break;
-        case "southwest":
-            handleDirection("southwest");
+        case 'southwest':
+            if (words.length > 1) {
+                outputText("I only understod you as far as southwest");
+            } else {
+                handleDirection("southwest");
+            }
         break;
-        case "west":
-            handleDirection("west");
+        case 'west':
+            if (words.length > 1) {
+                outputText("I only understod you as far as west");
+            } else {
+                handleDirection("west");
+            }
         break;
-        case "northwest":
-            handleDirection("northwest");
+        case 'northwest':
+            if (words.length > 1) {
+                outputText("I only understod you as far as northwest");
+            } else {
+                handleDirection("northwest");
+            }
         break;
-        case "look":
-            look();
+        case 'climb':
+            handleClimb(words);
         break;
-        case "jump":
-            jump();
+        case 'up':
+            outputText("How do you think you're going to do that?");
         break;
-        case "climb":
-            climb();
+        case 'down':
+            outputText("How do you think you're going to do that?");
         break;
         default:
-            promptSubject(verb);
-        break;
+            outputText("That's not a valid direction.");    //SHOULD NOT HAPPEN.
     }
+    return false;   //This makes it so handleVerb does not happen
 }
-/**
- * This variation of handleAction accepts a verb and one component
- * Only actions that require one component will be handled, otherwise
- * an error is reported to the user.
- */
-function handleVerbSubject(verb, component1) {
-    switch(verb) {
-        case "go":
-            handleDirection(component1);
-        break;
-        case "walk":
-            
-        break;
-        case "run":
 
-        break;
+function handleClimb(words) {
+    if (words.length == 1) {
+        handleDirection("up");
+    } else {
+        direction = words[1];
+        if (direction == 'up' || direction == 'down') {
+            if (words.length > 2) {
+                message = `I only understood you as far as climb ${direction}`;
+                outputText(message);
+            }
+            else {
+                handleDirection(direction);
+            }
+        }
     }
+    
 }
-/**
- * This variation of handleAction accepts a verb and two components.
- * Only actions that require two components will be handled, otherwise 
- * an error is reported to the user.
- */
-function handleVerbSubject1Subject2(verb, component1, component2) {
-    switch(verb) {
 
-    }
+/**
+ *  Assumes sentence works and performs action without checking for validity.
+ */
+function handleVerb(words) {
 }
 
 function handleDirection(direction) {
-    if(!currentRoom.directions.some(dir => {if(dir == direction) {index = currentRoom.directions.indexOf(direction);go(index); return true;}}))
+    if(!currentRoom.directions.some(dir => {
+        if(dir == direction) {
+            index = currentRoom.directions.indexOf(direction);
+            changeRoom(index); return true;
+        }
+    }))
         outputText("You can't go that direction");
 }
 
-function go(index) {
+function changeRoom(index) {
     currentRoom = currentRoom.connectedRooms[index];
     outputText(currentRoom.location);
-    outputText(currentRoom.description);
+    if (!currentRoom.entered) {
+        outputText(currentRoom.description);
+        currentRoom.entered = true;
+    }
     topRightElement.textContent = currentRoom.location;
 }
 
@@ -236,9 +309,6 @@ function jump() {
 }
 
 //In order to connect rooms that require climbing, up and down must be valid directions. However, they must only work when paired with climb
-function climb() {
-    outputText("Where do you want to climb?");
-}
 /**
  * Attacking something requires a few things:
  * 1. The thing you are attacking must have health
@@ -273,8 +343,10 @@ function outputText(txt) {
 
 window.onload = (event) => {
     initializeRooms();
+    outputText(currentRoom.location);
     outputText(currentRoom.description);
-};
+    currentRoom.entered = true;
+}
 
 
 
@@ -282,7 +354,7 @@ const terminalOutput = document.getElementById("terminal-output");
 const terminalCommand = document.getElementById("terminal-command");
 const topRightElement = document.getElementById("location");
 
-topRightElement.textContent = "North Road";
+topRightElement.textContent = "Brooke Road";
 
 terminalCommand.addEventListener("keydown", e => checkEnter(e));
 
@@ -294,7 +366,7 @@ function checkEnter(k) {
         if (command.length > 164) {
             outputText("I'm sorry, but that command is too long.")
         } else {
-        parse(command); 
+            parse(separateCommand(command)); 
         }
     }
 }
